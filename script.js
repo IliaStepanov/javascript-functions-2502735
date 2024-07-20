5abde7cba805f12d7b0ccf6ae5932062d5701966
Ilia Stepanov <istepanov@ru.estee.com>
2024-07-20 12:37:31 +0200

    solution

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 info = {
-  social: {
-    youtube: 'https://youtube.com/planetoftheweb',
-    linkedin: 'https://linkedin.com/in/planetoftheweb',
-    blog: 'https://raybo.org'
-  }
+    social: {
+        youtube: 'https://youtube.com/planetoftheweb',
+        linkedin: 'https://linkedin.com/in/planetoftheweb',
+        blog: 'https://raybo.org'
+    }
 }
 
-function listObject(data) {}
+function listObject(data) {
+    let querySelector = document.querySelector.bind(document);
+
+    let output = '';
+    for (let key in data) {
+        output += `${key}: ${data[key]}<br>`;
+    }
+    querySelector("#output").innerHTML = output;
+}
 
 listObject(info.social)
